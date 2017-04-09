a4b1e61c87c40d57df2f36864da48cb967ba7600
kaede-maple <kaede911002@gmail.com>
2017-04-09 17:43:13 +0900

    演習3.1のコードをリファクタリング

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,27 @@
 require 'test_helper'
-TITELE_STR = "Ruby on Rails Tutorial Sample App"
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | " + TITELE_STR 
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | " + TITELE_STR 
+    assert_select "title", "Help | #{@base_title}" 
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | " + TITELE_STR 
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
