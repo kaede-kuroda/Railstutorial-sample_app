f5ff4012b930a98781ae36866e23530315edbcfb
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-04-14 18:36:27 +0900

    ユーザー登録フォームの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,22 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
   end
+
+  def create
+    #@user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
