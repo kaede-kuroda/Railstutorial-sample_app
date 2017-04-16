ed797118b5cd5bc5216d9b5c0c91df03863d8437
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-04-17 08:17:41 +0900

    ログイン機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     #@user = User.new(params[:user])    # 実装は終わっていないことに注意!
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       # Handle a successful save.
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
