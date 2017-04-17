70de7513e5fdaffa74d0625295597d01c85316f1
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-04-17 20:37:28 +0900

    フレンドリーフォワーディングの実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,7 +30,7 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-  
+
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
@@ -48,5 +48,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
